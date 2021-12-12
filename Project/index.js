fd9450745b18b8d42fcfeac1a5196c901cafe934
danilKa04 <95904702+danilKa04@users.noreply.github.com>
2021-12-12 20:11:56 +0500

    Add files via upload

---
+++ Project/index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const url = require('url');
+const {app, BrowserWindow } = require('electron')
+let win;
+function createWindow(){
+
+     win = new BrowserWindow({
+        width: 700,
+        height: 700,
+        icon:__dirname + "./jpg/pic.jpg" 
+    })
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file",
+        slashes:true,
+    }))
+    win.on("closed", ()=>{
+        win = null;
+    })
+}
+app.on("ready", createWindow)
